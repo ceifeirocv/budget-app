3ce664725adc1a8aa9aeae41cc7eec258150cd4d
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-26 16:40:58 -0100

    add budgets

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,12 @@ import { useLoaderData } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { fetchData } from '../helpers';
 import Intro from '../components/Intro';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 export function dashboardLoader() {
   const userName = fetchData('userName');
-  return userName;
+  const budgets = fetchData('budgets');
+  return { userName, budgets };
 }
 
 export async function dashboardAction({ request }) {
@@ -20,11 +22,28 @@ export async function dashboardAction({ request }) {
 }
 
 function Dashboard() {
-  const userName = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      {userName ? (<p>{userName}</p>) : <Intro />}
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back,
+            {' '}
+            <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-small">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : <Intro />}
     </>
   );
 }
