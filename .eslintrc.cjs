6900934810fcc656cc398dc6273f264db9b127e2
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-26 17:55:32 -0100

    update prettier eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    "react/react-in-jsx-scope": "off",
   },
 };
