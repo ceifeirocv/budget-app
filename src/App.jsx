db6f6e177bb81f25871b4ee9e014243d68a541a9
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-26 23:16:47 -0100

    lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,47 @@
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // layouts
-import Main, { mainLoader } from "./layout/Main";
+import Main, { mainLoader } from './layout/Main';
 
 // Routes
-import Error from "./pages/Error";
-import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
-import { logoutAction  } from "./actions/logout";
+import Error from './pages/Error';
+import Dashboard, { dashboardAction, dashboardLoader } from './pages/Dashboard';
+import { logoutAction } from './actions/logout';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Main/>,
+    path: '/',
+    element: <Main />,
     loader: mainLoader,
     errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Dashboard/>,
+        element: <Dashboard />,
         loader: dashboardLoader,
         action: dashboardAction,
-        errorElement: <Error/>
+        errorElement: <Error />,
       },
       {
-        path: "logout",
+        path: 'logout',
         action: logoutAction,
-      }
-    ]
+      },
+    ],
   },
 ]);
 
-
 function App() {
-
-
   return (
     <div className="App">
       <RouterProvider router={router} />
       <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
