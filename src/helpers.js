bcc12c70b9c7171f2f99509b0f91a5f139f46bfa
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-05-28 17:13:23 -0100

    show budgets

--- src/helpers.js
+++ src/helpers.js
@@ -28,3 +28,23 @@ export const createExpense = ({ name, amount, budgetId }) => {
   };
   return localStorage.setItem('expenses', JSON.stringify([...existingExpense, newItem]));
 };
+
+export const formatCurrency = (amount) => amount.toLocaleString(undefined, {
+  style: 'currency',
+  currency: 'USD',
+});
+
+export const formatPercentage = (amount) => amount.toLocaleString(undefined, {
+  style: 'percent',
+  minimumFractionDigits: 0,
+});
+
+export const calculateSpentByBudget = (budjetId) => {
+  const expenses = fetchData('expenses') ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    if (expense.budgetId !== budjetId) return acc;
+
+    return acc + expense.amount;
+  }, 0);
+  return budgetSpent;
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { createBudget, createExpense, fetchData } from '../helpers';
 import Intro from '../components/Intro';
 import AddBudgetForm from '../components/AddBudgetForm';
 import AddExpenseForm from '../components/AddExpenseForm';
+import BudgetItem from '../components/BudgetItem';
 
 export function dashboardLoader() {
   const userName = fetchData('userName');
@@ -72,6 +73,14 @@ function Dashboard() {
                 <div className="flex-lg">
                   <AddBudgetForm />
                   <AddExpenseForm budgets={budgets} />
+                </div>
+                <h2>Existing Budgets</h2>
+                <div className="budgets">
+                  {
+                    budgets.map((budget) => (
+                      <BudgetItem key={budget.id} budget={budget} />
+                    ))
+                  }
                 </div>
               </div>
             ) : (
