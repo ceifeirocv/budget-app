42dbe371110b5cd0ca02076b553f5c3811bf8e5e
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-06-08 22:56:32 -0100

    create axios module

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/prefer-default-export */
-import axios from 'axios';
 import { redirect } from 'react-router-dom';
 import jwt from 'jwt-decode';
 import { toast } from 'react-toastify';
+import { api } from '../lib/axios';
 
 export const loginAction = async ({ request }) => {
   const data = await request.formData();
   const { accessToken } = Object.fromEntries(data);
 
   try {
-    const response = await axios.post('http://localhost:4000/login', {
+    const response = await api.post('/login', {
       accessToken,
     });
     const { token } = response.data;
