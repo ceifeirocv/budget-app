8ac2428e8499455438ffd061a7022b1cccb7acf6
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2023-06-15 17:48:15 -0100

    Revert "update to render server"
    This reverts commit 540c831cf21f8c0e279f29fd996b519f37ee5b6c.

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // eslint-disable-next-line import/prefer-default-export
 export const api = axios.create({
-  baseURL: 'https://budget-api-nn6x.onrender.com',
+  baseURL: 'http://localhost:4000',
 });
